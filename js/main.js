1dfc8c511e5209b9e0dcfd4139462399ff2f4045
Ryan Jacobs <ryanjacobs@ryans-mbp-3.hsd1.co.comcast.net>
2019-11-10 12:25:23 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,35 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
 console.log("User flipped" + cardOne);
 console.log("User flipped" + cardTwo);
+
+const cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = ['cardOne', 'cardTwo', 'cardThree', 'card Four'];
+
+function checkForMatch() {
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  console.log("You found a match!");
+} else {
+  console.log("Sorry, try again.");
+}
+}
+
+function flipCard(cardId) {
+	cardsInPlay.push(cards[cardId]);
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+}
+flipCard(0);
+flipCard(2);
+
+if (cardsInPlay[0] === cardsInPlay[1]) if (cardsInPlay.length === 2){
+	alert("You found a Match!");
+} else {
+	alert("Sorry, try again.");
+}
+
+console.log("up and running 2");
+flipCard(0);
+flipCard(1);
+
+
+
